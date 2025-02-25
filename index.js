b21fae748b66acd3b356059913e7c724d3219bd4
Marcus B Clements <mbc.uwl@icloud.com>
2025-02-24 22:33:29 -0600

    update styling and navigation functionality

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
+    loadNav();
+
+    const main = document.createElement('main');
+    const body = document.body;
+    body.appendChild(main);
+    
+});
+
+function loadNav() {
     const body = document.body;
 
     const header = document.createElement('header');
     const nav = document.createElement('nav');
 
-    const logo = document.createElement('div');
+    const logo = document.createElement('h1');
     logo.className = 'logo';
     logo.textContent = 'My Portfolio';
+    logo.addEventListener('click', () => {
+        window.location.href = '/';
+    });
 
     const navLinks = document.createElement('ul');
     navLinks.className = 'nav-links';
@@ -14,31 +26,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const links = ['About', 'Projects', 'Contact'];
     links.forEach(link => {
         const li = document.createElement('li');
-        const a = document.createElement('a');
-        a.href = `#${link.toLowerCase()}`;
-        a.textContent = link;
-        li.appendChild(a);
+        li.textContent = link.toLowerCase();
+        li.addEventListener('click', () => {
+            window.location.href = `#${link.toLowerCase()}`;
+        });
         navLinks.appendChild(li);
     });
 
     const burger = document.createElement('div');
     burger.className = 'burger';
-    for (let i = 0; i < 3; i++) {
+    const NUM_BURGER_LINES = 3;
+    for (let i = 0; i < NUM_BURGER_LINES; i++) {
         const div = document.createElement('div');
         div.className = `burger-line`;
         burger.appendChild(div);
     }
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
 
     nav.appendChild(logo);
     nav.appendChild(navLinks);
     nav.appendChild(burger);
     header.appendChild(nav);
     body.insertBefore(header, body.firstChild);
+}
 
-    const main = document.createElement('main');
-    body.appendChild(main);
-
-    burger.addEventListener('click', () => {
-        navLinks.classList.toggle('nav-active');
-    });
-});
